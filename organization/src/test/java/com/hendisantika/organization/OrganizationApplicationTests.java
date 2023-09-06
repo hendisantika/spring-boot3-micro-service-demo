5699741a46cba820893f8cc871a2cb9c5a1c39c9
hendisantika <hendisantika@yahoo.co.id>
2023-09-07 06:11:44 +0700

    #47 - Add Organization Service ✏️📗:octocat:⬆

---
+++ organization/src/test/java/com/hendisantika/organization/OrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
