6ea7c8d49ad203d59b1c16abbb560431a93f3489
hendisantika <hendisantika@yahoo.co.id>
2023-09-06 17:17:39 +0700

    #24 - Add StudentClient ✏️📗:octocat:⬆

---
+++ school/src/main/java/com/hendisantika/school/client/StudentClient.java
@@ -0,0 +1,25 @@
+package com.hendisantika.school.client;
+
+import com.hendisantika.school.entity.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-micro-service-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/6/23
+ * Time: 17:17
+ * To change this template use File | Settings | File Templates.
+ */
+@FeignClient(name = "student-service", url = "${application.config.students-url}")
+public interface StudentClient {
+
+    @GetMapping("/school/{school-id}")
+    List<Student> findAllStudentsBySchool(@PathVariable("school-id") Integer schoolId);
+}
