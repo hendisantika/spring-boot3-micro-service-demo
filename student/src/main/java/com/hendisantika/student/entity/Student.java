e6aef939130e38e056ff14c414805e5d6d164bad
hendisantika <hendisantika@yahoo.co.id>
2023-09-06 17:32:10 +0700

    #36 - Add Student Entity ✏️📗:octocat:⬆

---
+++ student/src/main/java/com/hendisantika/student/entity/Student.java
@@ -0,0 +1,33 @@
+package com.hendisantika.student.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-micro-service-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/6/23
+ * Time: 17:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Integer schoolId;
+}
