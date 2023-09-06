5d8e4b3ccd2bf2b2a1a3b44249d691f3afa6f6fa
hendisantika <hendisantika@yahoo.co.id>
2023-09-06 17:14:17 +0700

    #19 - Add Student Entity ✏️📗:octocat:⬆

---
+++ school/src/main/java/com/hendisantika/school/entity/Student.java
@@ -0,0 +1,24 @@
+package com.hendisantika.school.entity;
+
+import lombok.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-micro-service-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/6/23
+ * Time: 17:13
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private String firstname;
+    private String lastname;
+    private String email;
+}
