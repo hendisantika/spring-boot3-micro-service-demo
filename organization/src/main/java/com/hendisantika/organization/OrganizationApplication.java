9da4be14e391b06ad427ec30ac5f4d1c8ec6725f
hendisantika <hendisantika@yahoo.co.id>
2023-09-07 06:15:31 +0700

    #50 - Add configs in Main App ✏️📗:octocat:⬆

--- organization/src/main/java/com/hendisantika/organization/OrganizationApplication.java
+++ organization/src/main/java/com/hendisantika/organization/OrganizationApplication.java
@@ -1,9 +1,23 @@
 package com.hendisantika.organization;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@OpenAPIDefinition(
+		servers = {@Server(url = "/organitation", description = "Default Server Path URL")},
+		info = @Info(
+				title = "Organitation API",
+				version = "1.0",
+				description = "Documentation Organitation API v1.0"
+		))
 public class OrganizationApplication {
 
 	public static void main(String[] args) {
