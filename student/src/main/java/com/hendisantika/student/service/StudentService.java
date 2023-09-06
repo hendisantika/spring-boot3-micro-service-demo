5512d0634cebfd3d4d226c2d8089e2b6133a4d00
hendisantika <hendisantika@yahoo.co.id>
2023-09-06 19:14:09 +0700

    #41 - Add findAllStudentsBySchool in Student Service ✏️📗:octocat:⬆

--- student/src/main/java/com/hendisantika/student/service/StudentService.java
+++ student/src/main/java/com/hendisantika/student/service/StudentService.java
@@ -31,4 +31,8 @@ public class StudentService {
     public List<Student> findAllStudents() {
         return studentRepository.findAll();
     }
+
+    public List<Student> findAllStudentsBySchool(Integer schoolId) {
+        return studentRepository.findAllBySchoolId(schoolId);
+    }
 }
