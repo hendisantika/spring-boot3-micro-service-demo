3e563b69c831fb86286b35df2183a4c95a54a6de
hendisantika <hendisantika@yahoo.co.id>
2023-09-07 06:18:35 +0700

    #52 - Add Departement Service ✏️📗:octocat:⬆

---
+++ departement/src/test/java/com/hendisantika/departement/DepartementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.departement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
