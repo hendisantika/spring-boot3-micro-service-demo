f3f55ebfeb5f564afe9fdbf6c378e4a79901387c
hendisantika <hendisantika@yahoo.co.id>
2023-09-06 19:13:04 +0700

    #38 - Add Student Service ✏️📗:octocat:⬆

---
+++ student/src/main/java/com/hendisantika/student/service/StudentService.java
@@ -0,0 +1,23 @@
+package com.hendisantika.student.service;
+
+import com.hendisantika.student.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-micro-service-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/6/23
+ * Time: 19:12
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+}
