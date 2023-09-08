d04aff6a813fc6e056da34a2703d5a48f2417863
hendisantika <hendisantika@yahoo.co.id>
2023-09-08 07:56:50 +0700

    #60 - Add SpringDocs API for Employee Service ✏️:octocat:⬆

--- employee/src/main/java/com/hendisantika/employee/EmployeeApplication.java
+++ employee/src/main/java/com/hendisantika/employee/EmployeeApplication.java
@@ -1,9 +1,21 @@
 package com.hendisantika.employee;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(
+		servers = {@Server(url = "/employee", description = "Default Server Path URL")},
+		info = @Info(
+				title = "Employee API",
+				version = "1.0",
+				description = "Documentation Employee API v1.0"
+		))
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
