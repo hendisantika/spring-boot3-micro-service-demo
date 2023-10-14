da8c8a56a7f5d9de64e05e7b29d9ca42194c9bbf
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-14 09:28:59 +0700

    #66 - Set Spring Docs OpenAPI in organization service ✏️📗:octocat:⬆

--- organization/src/main/java/com/hendisantika/organization/OrganizationApplication.java
+++ organization/src/main/java/com/hendisantika/organization/OrganizationApplication.java
@@ -1,8 +1,9 @@
 package com.hendisantika.organization;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,13 +12,18 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@OpenAPIDefinition(
-		servers = {@Server(url = "/organitation", description = "Default Server Path URL")},
-		info = @Info(
-				title = "Organitation API",
-				version = "1.0",
-				description = "Documentation Organitation API v1.0"
-		))
+@OpenAPIDefinition(info =
+@Info(
+		title = "Organization API",
+		version = "1.0",
+		description = "Documentation Organization API v1.0",
+		contact = @Contact(
+				name = "Hendi Santika",
+				url = "https://s.id/hendisantika",
+				email = "hendisantika@yahoo.co.id"
+		),
+		license = @License(name = "Apache 2.0", url = "http://foo.bar"))
+)
 public class OrganizationApplication {
 
 	public static void main(String[] args) {
