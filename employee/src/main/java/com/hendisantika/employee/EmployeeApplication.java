5a14540e09ea09798081060936465513c5bb78a3
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 13:39:57 +0700

    #76 - Bug Fix config 🫸🌀✏️📗 :octocat:🐧🐳

--- employee/src/main/java/com/hendisantika/employee/EmployeeApplication.java
+++ employee/src/main/java/com/hendisantika/employee/EmployeeApplication.java
@@ -1,18 +1,19 @@
 package com.hendisantika.employee;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @OpenAPIDefinition(
 		servers = {@Server(url = "/employee", description = "Default Server Path URL")},
-		info = @Info(
-				title = "Employee API",
-				version = "1.0",
-				description = "Documentation Employee API v1.0"
-		))
-@OpenAPIDefinition(info =
+		info =
 @Info(
 		title = "Employee API",
 		version = "1.0",
--- gateway/src/main/java/com/hendisantika/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/hendisantika/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
